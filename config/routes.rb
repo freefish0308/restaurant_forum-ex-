6112129f7b0eb86019a54b8cf250fbb0fc63486d
Kafka Tzeng <freefish0308@gmail.com>
2018-06-17 23:12:22 +0800

    setup category resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :restaurants, only: [:index, :show]
+  resources :categories, only: :show
+
   root "restaurants#index"
 
   namespace :admin do
