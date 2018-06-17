347b9d3c9bc77aa2d4816c7fe6e5ae3159c5a938
Kafka Tzeng <freefish0308@gmail.com>
2018-06-17 22:52:17 +0800

    Revert "<div class=container>"
    This reverts commit 9343e2a4a14331e5d44db9d77802310791f40bdd.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,8 +3,4 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.page(params[:page]).per(9)
   end
-
-  def show
-    @restaurant = Restaurant.find(params[:id])
-  end
 end
