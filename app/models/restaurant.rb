9b69083ffbb5dcee02cea1e809479d9bf855b093
Kafka Tzeng <freefish0308@gmail.com>
2018-06-30 15:54:21 +0800

    S16 L1U1-U2 all

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,9 @@
 class Restaurant < ApplicationRecord
   mount_uploader :image, PhotoUploader
   validates_presence_of :name
+  
   belongs_to :category
 
-  has_many :comments
+  # 當 Restaurant 物件被刪除時，順便刪除依賴的 Comment
+  has_many :comments, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,23 @@ Rails.application.routes.draw do
 
   resources :restaurants, only: [:index, :show] do
     resources :comments, only: [:create, :destroy]
-    
+
+    collection do
+      # 瀏覽所有餐廳的最新動態
+      # GET restaurants/feeds
+      get :feeds
+    end
+
+    member do
+      # 瀏覽個別餐廳的 Dashboard
+      # GET restaurants/:id/dashboard
+      get :dashboard
+    end
   end
 
   resources :users, only: [:show, :edit, :update]
 
   resources :categories, only: :show
-
   root "restaurants#index"
 
   namespace :admin do
