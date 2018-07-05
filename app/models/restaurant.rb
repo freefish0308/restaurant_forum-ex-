584b6900bab3cdea434e9f5923a2950ff8213988
Kafka Tzeng <freefish0308@gmail.com>
2018-07-05 14:13:56 +0800

    setup relation among user, restaurant and like

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -13,6 +13,12 @@ class Restaurant < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
 
+  # 「餐廳被很多使用者收藏」的多對多關聯
+  # 將關聯名稱自訂為 :liked_users
+  # 自訂名稱後，Rails 無法自動推論來源名稱，需另加 source 告知 model name
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
+
   def is_favorited?(user)
     self.favorited_users.include?(user)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,11 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_restaurants, through: :favorites, source: :restaurant
  
+  # 「使用者收藏很多餐廳」的多對多關聯
+  # 由於使用 restaurants 會和「使用者評論很多餐廳」重覆，將關聯名稱自訂為 :liked_restaurants
+  # 自訂名稱後，Rails 無法自動推論來源名稱，需另加 source 告知 model name
+  has_many :likes, dependent: :destroy
+  has_many :liked_restaurants, through: :likes, source: :restaurant
 
   # admin? 讓我們用來判斷單個user是否有 admin 角色，列如：current_user.admin?
   def admin?
