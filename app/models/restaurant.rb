43713a4ceda1085daf55c07e056badd61c74dc05
Kafka Tzeng <freefish0308@gmail.com>
2018-07-06 13:26:41 +0800

    add like/unlike

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -13,7 +13,7 @@ class Restaurant < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
 
-  # 「餐廳被很多使用者收藏」的多對多關聯
+  # 「餐廳被很多使用者喜歡」的多對多關聯
   # 將關聯名稱自訂為 :liked_users
   # 自訂名稱後，Rails 無法自動推論來源名稱，需另加 source 告知 model name
   has_many :likes, dependent: :destroy
@@ -21,6 +21,10 @@ class Restaurant < ApplicationRecord
 
   def is_favorited?(user)
     self.favorited_users.include?(user)
+  end
+
+  def is_liked?(user)
+    self.liked_users.include?(user)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_restaurants, through: :favorites, source: :restaurant
  
-  # 「使用者收藏很多餐廳」的多對多關聯
+  # 「使用者喜歡很多餐廳」的多對多關聯
   # 由於使用 restaurants 會和「使用者評論很多餐廳」重覆，將關聯名稱自訂為 :liked_restaurants
   # 自訂名稱後，Rails 無法自動推論來源名稱，需另加 source 告知 model name
   has_many :likes, dependent: :destroy
