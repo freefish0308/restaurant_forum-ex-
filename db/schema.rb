1282436a51629f68906766bccf50932d7653b34b
Kafka Tzeng <freefish0308@gmail.com>
2018-07-10 15:09:44 +0800

    create followship model&freindship table relationships

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20180710062922) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "following_id"
+    t.integer "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
